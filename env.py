2119b2f01f60e93fbe41a219160f455014d58701
Pranshu-S <pranshushukla06@gmail.com>
2021-01-25 15:31:18 +0400

    env setting

---
+++ env.py
@@ -0,0 +1,27 @@
+class env:
+    def __init__(self):
+        self.x_range = 20
+        self.y_range = 20
+        self.motions = [(-1, 0), (-1, 1), (0, 1), (1, 1),
+                        (1, 0), (1, -1), (0, -1), (-1, -1)]
+        self.obs = self.obs_map()
+
+    def update_obs(self, obs):
+        self.obs = obs
+
+    def obs_map(self):
+        x = self.x_range
+        y = self.y_range
+        obs = set()
+
+        for i in range(x):
+            obs.add((i, 0))
+        for i in range(x):
+            obs.add((i, y - 1))
+
+        for i in range(y):
+            obs.add((0, i))
+        for i in range(y):
+            obs.add((x - 1, i))
+
+        return obs
