b2c5bead3336a31372369d1f3aff3731da0343fd
Pranshu-S <pranshushukla06@gmail.com>
2021-02-03 08:28:32 +0400

    Adding plotting file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Path-Planning-Algos
 An attempt to simulate Path Planning algorithms using Python
+
+Current files - 
+env.py - (class env)initializes obstacles and range
+plotting.py - (class Plotting)creates plots
--- plotting.py
+++ plotting.py
@@ -1,3 +1,50 @@
 import os
 import sys
 import matplotlib.pyplot as plt
+import env
+
+class Plotting:
+    def __init__(self, xI, xG):
+        self.xI, self.xG = xI,xG
+        self.env = env.env()
+        self.obs = self.env.obs_map()
+
+    def plot_grid(self, name):
+        obs_x = [x[0] for x in self.obs]
+        obs_y = [x[1] for x in self.obs]
+
+        plt.plot(self.xI[0], self.xI[1], "bs")
+        plt.plot(self.xG[0], self.xG[1], "gs")
+        plt.plot(obs_x,obs_y,"sk")
+        plt.title(name)
+        plt.axis("equal")
+
+    @staticmethod
+    def color_list():
+        cl_v = ['silver',
+                'wheat',
+                'lightskyblue',
+                'royalblue',
+                'slategray']
+        cl_p = ['gray',
+                'orange',
+                'deepskyblue',
+                'red',
+                'm']
+        return cl_v, cl_p
+
+    @staticmethod
+    def color_list_2():
+        cl = ['silver',
+              'steelblue',
+              'dimgray',
+              'cornflowerblue',
+              'dodgerblue',
+              'royalblue',
+              'plum',
+              'mediumslateblue',
+              'mediumpurple',
+              'blueviolet',
+              ]
+        return cl
+        
